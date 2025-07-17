15a2c00b752ac88b99aef5849efcb6bf42a49d96
Theone Eclarin <theoneeclarin@outlook.com>
2025-07-17 23:17:02 +0800

    fix: avoid returning sensitive error messages to client

--- src/server/error/api-error.ts
+++ src/server/error/api-error.ts
@@ -2,13 +2,20 @@
  * General-purpose error class for throwing errors with an HTTP status code
  */
 export class ApiError extends Error {
+  /**
+   * @param status - HTTP status code to return
+   */
   constructor(status: number);
+
+  /**
+   * @param message - A safe message to show to the user. AVOID including sensitive information.
+   */
   constructor(status: number, message?: string);
   constructor(status: number, message?: string, options?: ErrorOptions);
   constructor(
     public status: number,
     message?: string,
-    options?: ErrorOptions,
+    options?: ErrorOptions
   ) {
     super(message, options);
   }
--- src/server/error/error-handler.ts
+++ src/server/error/error-handler.ts
@@ -9,12 +9,18 @@ export function errorHandler(error: unknown, request: TsRestRequest) {
 
   if (error instanceof Error) {
     const status = error instanceof ApiError ? error.status : 500;
-    const body = error.message ? { message: error.message } : null;
+
+    // If error is `ApiError`, we assume the message is safe (not sensitive) to show to the user.
+    const body =
+      error instanceof ApiError
+        ? { message: error.message }
+        : { message: "An internal server error ocurred." };
+
     return TsRestResponse.fromJson(body, { status });
   }
 
   return TsRestResponse.fromJson(
     { message: "An unexpected error ocurred." },
-    { status: 500 },
+    { status: 500 }
   );
 }
